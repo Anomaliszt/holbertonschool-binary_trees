7c69d4994c78ee2b0b40978cf8dc0c088d236e53
Saki <saki@LeHote.myguest.virtualboxorg>
2024-08-06 16:41:17 +0200

    tentative fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,20 +10,18 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *right_child = malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+		return (NULL);
+	binary_tree_t *right_child = binary_tree_node(parent, value);
 
 	if (right_child == NULL)
 		return (NULL);
-	if (parent == NULL)
-		return (NULL);
-
-	right_child->n = value;
-	right_child->parent = parent;
-	right_child->left = NULL;
-	right_child->right = parent->right;
 
 	if (parent->right)
+	{
 		parent->right->parent = right_child;
+		right_child->right = parent->right;
+	}
 
 	parent->right = right_child;
 
