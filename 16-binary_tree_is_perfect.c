19b76d934ff9c7ce07a23407c2bd726816bd0dfb
Anomaliszt <xavier.sanchez44@orange.fr>
2024-08-07 08:58:17 +0000

    Add function 18

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_is_perfect - func that checks if binary tree is perfect
+* @tree: pointer to root node of tree to check
 *
-*
-*
-* Return: 
+* Return: 0 if tree is NULL, else int
 */
 
-
-
-{ 
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
 
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,14 +1,25 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_uncle - func that finds the uncle of a node
+* @node: pointer to the node to find uncle
 *
-*
-*
-* Return: 
+* Return: pointer to the uncle node or NULL
 */
 
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandpa;
 
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
 
-{ 
+	grandpa = node->parent->parent;
+	if(grandpa->left == node->parent)
+		return (grandpa->right);
 
+	if(grandpa->right == node->parent)
+		return (grandpa->left);
+
+	return (NULL);
 }
